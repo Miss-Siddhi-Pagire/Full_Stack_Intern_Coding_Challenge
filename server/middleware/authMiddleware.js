e0b0a60c14c4fe572a1b796e52d48c347e75e83a
Miss-Siddhi-Pagire <siddhipagire825@gmail.com>
2026-05-15 20:35:26 +0530

    Create JWT authentication middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const { verifyToken } = require('../utils/jwtUtils');
+
+const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ success: false, message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  const decoded = verifyToken(token);
+
+  if (!decoded) {
+    return res.status(401).json({ success: false, message: 'Invalid or expired token' });
+  }
+
+  req.user = decoded;
+  next();
+};
+
+module.exports = authenticate;
