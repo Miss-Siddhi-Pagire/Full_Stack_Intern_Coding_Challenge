4b4e2c3faa60c01896cb9da35a90d74beb207a84
Miss-Siddhi-Pagire <siddhipagire825@gmail.com>
2026-05-15 20:34:55 +0530

    Implement user login API endpoint

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel');
-const { hashPassword } = require('../utils/passwordUtils');
 const { validationResult } = require('express-validator');
+const { generateToken } = require('../utils/jwtUtils');
+const { hashPassword, comparePassword } = require('../utils/passwordUtils');
 
 const register = async (req, res, next) => {
   try {
@@ -35,6 +36,46 @@ const register = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ success: false, errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findByEmail(email);
+    if (!user) {
+      return res.status(401).json({ success: false, message: 'Invalid credentials' });
+    }
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ success: false, message: 'Invalid credentials' });
+    }
+
+    const token = generateToken({ id: user.id, role: user.role });
+
+    res.status(200).json({
+      success: true,
+      message: 'Login successful',
+      data: {
+        token,
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          role: user.role
+        }
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
-  register
+  register,
+  login
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const { registrationRules } = require('../utils/validationRules');
+const { registrationRules, loginRules } = require('../utils/validationRules');
 
 router.post('/register', registrationRules, authController.register);
+router.post('/login', loginRules, authController.login);
 
 module.exports = router;
