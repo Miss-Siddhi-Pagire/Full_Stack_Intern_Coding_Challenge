defbf249c529f604f9279cf8dace8d241760f62b
Miss-Siddhi-Pagire <siddhipagire825@gmail.com>
2026-05-15 20:30:35 +0530

    Define common input validation patterns

---
+++ server/utils/validationRules.js
@@ -0,0 +1,28 @@
+const { body } = require('express-validator');
+
+const registrationRules = [
+  body('name')
+    .isLength({ min: 20, max: 60 })
+    .withMessage('Name must be between 20 and 60 characters'),
+  body('email')
+    .isEmail()
+    .withMessage('Must be a valid email address'),
+  body('address')
+    .isLength({ max: 400 })
+    .withMessage('Address cannot exceed 400 characters'),
+  body('password')
+    .isLength({ min: 8, max: 16 })
+    .withMessage('Password must be 8-16 characters')
+    .matches(/^(?=.*[A-Z])(?=.*[!@#$%^&*])/)
+    .withMessage('Password must include at least one uppercase letter and one special character')
+];
+
+const loginRules = [
+  body('email').isEmail().withMessage('Valid email is required'),
+  body('password').notEmpty().withMessage('Password is required')
+];
+
+module.exports = {
+  registrationRules,
+  loginRules
+};
